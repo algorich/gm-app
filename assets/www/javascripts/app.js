2274ee1c8bd2ea6ecf181f6710779044dbe356e0
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-06-22 11:50:45 -0300

    Add keyboard

--- assets/www/javascripts/app.js
+++ assets/www/javascripts/app.js
@@ -1,3 +1,23 @@
 var DB = Lawnchair({name:'goodbyemoney'}, function(){});
 
-function onDeviceReady() {};
+/* Virify if the phone load PhoneGap */
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+};
+
+/* Handle events. */
+$(function(){
+  $('#keyboard').on('click', '.digit', function(){
+      digit = $(this).text();
+      $('#value').text(function(index, text){
+        return text + digit;
+      });
+  });
+
+  $('#keyboard .del').click(function(){
+      $('#value').text(function(index, text){
+        return text.slice(0, -1);
+      });
+  });
+});
