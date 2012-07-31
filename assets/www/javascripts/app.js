af04eda8f0c296b88acd387272494e83a61055d6
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-07-31 01:46:52 -0300

    Basic version

--- assets/www/javascripts/app.js
+++ assets/www/javascripts/app.js
@@ -1,26 +1,49 @@
+/* set database */
 var DB = Lawnchair({name:'goodbyemoney'}, function(){});
+
+/* jquery mobile config */
+$.mobile.page.prototype.options.addBackBtn = true;
+$.mobile.page.prototype.options.backBtnText = "voltar";
 
 /* Virify if the phone load PhoneGap */
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+    document.addEventListener("menubutton", onMenuButtonDown, false);
+};
+
+function onMenuButtonDown(){
+    updateList();
+    $.mobile.changePage("#page-list");
 };
 
 /* Handle events. */
-/* TODO: refactoring based on:
-    http://floatlearning.com/2011/03/developing-better-phonegap-apps/
-    http://developers.google.com/mobile/articles/fast_buttons */
 $(function(){
-  $('#keyboard').on('touchend', '.digit', function(){
-      digit = $(this).text();
-      $('#value').text(function(index, text){
-        return text + digit;
-      });
-  });
-
-  $('#keyboard').on('touchend', '.del', function(){
-      $('#value').text(function(index, text){
-        return text.slice(0, -1);
-      });
-  });
+    $('#say-goodbye').on('tap', function(){
+        var amount = $('[name="amount"]').val(),
+            desc = $('[name="description"]').val(),
+            date = new Date();
+        DB.save({
+            key: date.getTime(),
+            value: { amount: amount, description: desc, date: date.toString() }
+        });
+        navigator.app.exitApp();
+    });
 });
+
+/* update the spending list. */
+function updateList() {
+    ul = $('#spending-list');
+    DB.all(function(list) {
+        /* order spending list by date and time */
+        list.sort(function(a,b){ return b.key - a.key; });
+        ul.html('');
+        list.forEach(function(spent) {
+            ul.append('<li>' +
+                        '<h3>R$ ' + spent.value.amount + '</h3>' +
+                        '<p><strong>' + spent.value.description + '</strong></p>' +
+                        '<p>' + spent.value.date + '</p>' +
+                      '</li>');
+        });
+    });
+};
