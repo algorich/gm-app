c9f892f5bd4c4b07d4774700593369514a9e1af8
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-09-07 13:32:43 -0300

    sum the mount spent. close #7

--- assets/www/app/store/Spents.js
+++ assets/www/app/store/Spents.js
@@ -12,15 +12,30 @@ Ext.define('GoodbyeMoney.store.Spents', {
             }
         ],
 
+        //
+        // TODO: Find a better way to add the total spent on the group header.
+        // Run the store.each loop for every record on the store sucks.
+        //
         grouper: {
             direction: 'DESC',
             groupFn: function(record) {
                 var mounthNames = ['January', 'February', 'March', 'April',
                     'May', 'June', 'July', 'August', 'September', 'October',
                     'November', 'December'];
+                var total = 0;
+                var store = record.stores[0];
                 var date = record.get('date');
+                var title = date.getFullYear() + ', ' + mounthNames[date.getMonth()];
 
-                return date.getFullYear() + ', ' + mounthNames[date.getMonth()];
+                var template = new Ext.Template(
+                    '{title} <span class="total-amount">total: $ {total}</span>'
+                );
+
+                store.each(function(item) {
+                    total += item.get('amount');
+                });
+
+                return template.apply({title: title, total: total.toFixed(2)});
             }
         }
     }
